7bc5dc2f5bd79e6148114644967ad24a61972345
Kasun Gimantha <97676779+KasunGimantha@users.noreply.github.com>
2022-01-17 07:33:04 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,26 @@
+package com.KaZuN;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Price of an item($): ");
+        int priceOfAnItem = scanner.nextInt();
+
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Production cost of an item($): ");
+        int costOfAnItem = scanner2.nextInt();
+
+        Scanner scanner3 = new Scanner(System.in);
+        System.out.println("Number of items you sell: ");
+        int numberOfItemsSell = scanner3.nextInt();
+
+        System.out.println("Profit: ");
+        System.out.println((priceOfAnItem-costOfAnItem)*numberOfItemsSell +"$");
+
+    }
+
+}
